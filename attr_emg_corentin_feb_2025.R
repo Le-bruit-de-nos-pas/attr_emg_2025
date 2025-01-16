51c6f5fd30c04a792cd13ccbefa68f3a6994e3d6
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2025-01-16 18:56:20 +0000

    Add files via upload

---
+++ attr_emg_corentin_feb_2025.R
@@ -0,0 +1,126 @@
+library(readxl)
+library(tidyverse)
+library(data.table)
+library(missMDA)
+
+# Summary statistics for target variables: raw and imputed --------------------------------------------
+
+attr_emg_input <- fread("../data/attr_emg_input.txt")
+
+names(attr_emg_input)
+
+unique(attr_emg_input$Enrolled)
+
+attr_emg_input <- attr_emg_input %>% filter(is.na(Enrolled))
+
+df_target_vars <- attr_emg_input %>% select(NISLL, FAP, MedianMotorRight_Wrist_ThumbAbduction:Score_UlnSPI)
+
+# Create an empty data frame to store results
+summary_table <- data.frame(
+  Variable = character(),
+  Mean = numeric(),
+  SD = numeric(),
+  Median = numeric(),
+  Q1 = numeric(),
+  Q3 = numeric(),
+  stringsAsFactors = FALSE
+)
+
+# Loop through each column in the data frame
+for (col_name in names(df_target_vars)) {
+  
+  # Extract the column
+  col_data <- df_target_vars[[col_name]]
+  
+  # Calculate statistics while ignoring NAs
+  col_stats <- data.frame(
+    Variable = col_name,
+    Mean = round(mean(col_data, na.rm = TRUE),2),
+    SD = round(sd(col_data, na.rm = TRUE),2),
+    Median = round(median(col_data, na.rm = TRUE),2),
+    Q1 = round(quantile(col_data, probs = 0.25, na.rm = TRUE),2),
+    Q3 = round(quantile(col_data, probs = 0.75, na.rm = TRUE),2)
+  )
+  
+  # Append the statistics to the summary table
+  summary_table <- rbind(summary_table, col_stats)
+}
+
+# Print the summary table
+print(summary_table)
+
+
+summary_table %>% 
+  mutate(mean=paste0(Mean, " ± ", SD), 
+         median=paste0(Median, " [", Q1, "-", Q3, "]")) %>% 
+  select(Variable, mean, median)
+
+
+
+df_target_vars %>% drop_na() # 63 left 63/453 14% only, not a chance
+
+
+# Impute missing values using PCA
+
+sum(is.na(df_target_vars)) / (dim(df_target_vars)[1] * dim(df_target_vars)[2])
+
+df_target_vars_imputed <- imputePCA(df_target_vars,ncp=20, scale = T)
+
+df_target_vars_imputed <- data.frame(df_target_vars_imputed$completeObs)
+
+# Create an empty data frame to store results
+summary_table <- data.frame(
+  Variable = character(),
+  Mean = numeric(),
+  SD = numeric(),
+  Median = numeric(),
+  Q1 = numeric(),
+  Q3 = numeric(),
+  stringsAsFactors = FALSE
+)
+
+# Loop through each column in the data frame
+for (col_name in names(df_target_vars_imputed)) {
+  
+  # Extract the column
+  col_data <- df_target_vars_imputed[[col_name]]
+  
+  # Calculate statistics while ignoring NAs
+  col_stats <- data.frame(
+    Variable = col_name,
+    Mean = round(mean(col_data, na.rm = TRUE),2),
+    SD = round(sd(col_data, na.rm = TRUE),2),
+    Median = round(median(col_data, na.rm = TRUE),2),
+    Q1 = round(quantile(col_data, probs = 0.25, na.rm = TRUE),2),
+    Q3 = round(quantile(col_data, probs = 0.75, na.rm = TRUE),2)
+  )
+  
+  # Append the statistics to the summary table
+  summary_table <- rbind(summary_table, col_stats)
+}
+
+# Print the summary table
+print(summary_table)
+
+
+
+sum(df_target_vars_imputed<0) / (dim(df_target_vars_imputed)[1] * dim(df_target_vars_imputed)[2])
+
+df_target_vars_imputed[df_target_vars_imputed<0] <- 0
+
+summary_table %>% 
+  mutate(mean=paste0(Mean, " ± ", SD), 
+         median=paste0(Median, " [", Q1, "-", Q3, "]")) %>% 
+  select(Variable, mean, median)
+
+
+
+df_target_vars
+df_target_vars_imputed
+
+fwrite(df_target_vars, "../data/df_target_vars.txt")
+fwrite(df_target_vars_imputed, "../data/df_target_vars_imputed.txt")
+
+
+
+# ----------
